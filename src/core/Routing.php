e4937c4ffe1eac3e2d97fd7ab2f516ea301f0677
Daniel Andre Eikeland <zegenie@zegeniestudios.net>
2020-05-11 16:34:29 +0200

    fix php deprecated accessor

--- src/core/Routing.php
+++ src/core/Routing.php
@@ -279,7 +279,7 @@
 					$elements[count($elements) - 1] = $matches[1];
 					$route = '/'.implode('/', $elements);
 				}
-				else if ($route{mb_strlen($route) - 1} == '/')
+				else if (mb_substr($route, -1) == '/')
 				{
 					$suffix = '/';
 				}
